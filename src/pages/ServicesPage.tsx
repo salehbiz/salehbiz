ea319e698d307d90fa63d0cab2b715661cff5e67
salehbiz <itsmuhammadsal@gmail.com>
2026-05-31 00:32:24 +0400

    fix: import HomePage.css into ServicesPage.tsx to apply layout and particle core styles

--- src/pages/ServicesPage.tsx
+++ src/pages/ServicesPage.tsx
@@ -2,6 +2,7 @@ import React, { useEffect, useState, useRef } from 'react';
 import { useNavigate } from 'react-router-dom';
 import RevealText from '../components/RevealText';
 import Navbar from '../components/Navbar';
+import './HomePage.css';
 import './ServicesPage.css';
 
 const ServicesPage: React.FC = () => {
